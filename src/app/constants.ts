8ec72f7d94418c72d651df1fa2f88c2a07ad66bb
opview <lynnzeng0101@gmail.com>
2024-04-15 01:12:47 +0800

    change dashboard

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,7 +3,7 @@ export const API_URL = `${import.meta.env.SITE}${import.meta.env.BASE_URL}api/`;
 // export const REMOTE_ASSETS_BASE_URL = `https://www.opview.xyz`;
 export const REMOTE_ASSETS_BASE_URL = `http://localhost:2121`;
 
-export const SITE_TITLE = 'Flowbite Astro Admin Dashboard';
+export const SITE_TITLE = 'Optimistic View';
 
 /* Useful flag for sourcing from `./data` entirely, disabling randomize layer */
 export const RANDOMIZE = Boolean(import.meta.env.RANDOMIZE) || true;
